f9a876971c3150870364ae88a7de2efccb8eb007
Mariana Moreira <marianaccmoreira@gmail.com>
2021-06-22 12:30:29 +0100

    rocket ship

--- rocket-ship/src/routes/LaunchPad/components/LaunchPad.js
+++ rocket-ship/src/routes/LaunchPad/components/LaunchPad.js
@@ -1,11 +1,15 @@
-import React, { useState } from 'react';
-import { ClassRocket, FunctionalRocket } from './Rocket';
-import '../styles/_launchpad.scss';
+import React, { useState, useEffect } from "react";
+import { ClassRocket, FunctionalRocket } from "./Rocket";
+import "../styles/_launchpad.scss";
 
 export default function LaunchPad() {
   const [rerenderCount, triggerRerender] = useState(0);
 
-  setTimeout(() => { triggerRerender(rerenderCount + 1); }, 500);
+  useEffect(() => {
+    setTimeout(() => triggerRerender(rerenderCount + 1), 0);
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
     <div className="launchpad">
