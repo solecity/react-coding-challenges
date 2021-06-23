5b3e6c6d194e4a899c615751bf42abf2a8e11977
Mariana Moreira <marianaccmoreira@gmail.com>
2021-06-23 18:11:49 +0100

    chatter

--- chatter/src/components/Messages/components/Footer.js
+++ chatter/src/components/Messages/components/Footer.js
@@ -1,13 +1,15 @@
-import React from 'react';
+import React from "react";
 
 const RETURN_KEY_CODE = 13;
 
 export default function Footer({ sendMessage, onChangeMessage, message }) {
   const onKeyDown = ({ keyCode }) => {
-    if (keyCode !== RETURN_KEY_CODE ) { return; }
+    if (keyCode !== RETURN_KEY_CODE) {
+      return;
+    }
 
     sendMessage();
-  }
+  };
 
   return (
     <div className="messages__footer">
@@ -16,12 +18,15 @@ export default function Footer({ sendMessage, onChangeMessage, message }) {
         placeholder="Write a message..."
         id="user-message-input"
         onChange={onChangeMessage}
+        value={message}
       />
       <div className="messages__footer__actions">
         <i className="far fa-smile" />
         <i className="fas fa-paperclip" />
         <i className="mdi mdi-ticket-outline" />
-        <button onClick={sendMessage} disabled={!message}>Send</button>
+        <button onClick={sendMessage} disabled={!message}>
+          Send
+        </button>
       </div>
     </div>
   );
--- chatter/src/components/Messages/components/Messages.js
+++ chatter/src/components/Messages/components/Messages.js
@@ -1,30 +1,88 @@
-import React, { useContext } from 'react';
-import io from 'socket.io-client';
-import useSound from 'use-sound';
-import config from '../../../config';
-import LatestMessagesContext from '../../../contexts/LatestMessages/LatestMessages';
-import TypingMessage from './TypingMessage';
-import Header from './Header';
-import Footer from './Footer';
-import Message from './Message';
-import '../styles/_messages.scss';
+import React, { useContext, useState, useEffect, useCallback } from "react";
+import io from "socket.io-client";
+import useSound from "use-sound";
+import config from "../../../config";
+import LatestMessagesContext from "../../../contexts/LatestMessages/LatestMessages";
+import TypingMessage from "./TypingMessage";
+import Header from "./Header";
+import Footer from "./Footer";
+import Message from "./Message";
+import "../styles/_messages.scss";
+import INITIAL_BOTTY_MESSAGE from "../../../common/constants/initialBottyMessage";
 
-const socket = io(
-  config.BOT_SERVER_ENDPOINT,
-  { transports: ['websocket', 'polling', 'flashsocket'] }
-);
+const socket = io(config.BOT_SERVER_ENDPOINT, {
+  transports: ["websocket", "polling", "flashsocket"],
+});
 
 function Messages() {
   const [playSend] = useSound(config.SEND_AUDIO_URL);
   const [playReceive] = useSound(config.RECEIVE_AUDIO_URL);
   const { setLatestMessage } = useContext(LatestMessagesContext);
 
+  const [message, setMessage] = useState("");
+  const [messageList, setMessageList] = useState([
+    { user: "bot", message: INITIAL_BOTTY_MESSAGE },
+  ]);
+  const [isTyping, setIsTyping] = useState(false);
+  const [isBotTyping, setIsBotTyping] = useState(false);
+
+  socket.on(
+    "bot-message",
+    useCallback(
+      (message) => {
+        setIsTyping(false);
+        setIsBotTyping(false);
+        setLatestMessage("bot", message);
+        setMessageList([...messageList, { user: "bot", message: message }]);
+      },
+      [messageList, setLatestMessage]
+    )
+  );
+
+  socket.on(
+    "bot-typing",
+    useCallback(() => {
+      setIsTyping(true);
+      setIsBotTyping(true);
+    }, [])
+  );
+
+  const sendMessage = useCallback(() => {
+    socket.emit("user-message", message);
+
+    setLatestMessage("me", message);
+    setMessageList([...messageList, { user: "me", message: message }]);
+    setMessage("");
+  }, [message, messageList, setLatestMessage]);
+
+  const onChangeMessage = useCallback((e) => setMessage(e.target.value), []);
+
+  const generateMessageList = messageList.map((msg, index) => (
+    <Message
+      key={index}
+      message={{ ...msg, id: index }}
+      botTyping={isBotTyping}
+    />
+  ));
+
+  useEffect(() => {
+    document
+      .getElementById("message-list")
+      .scrollTo(0, document.getElementById("message-list").scrollHeight);
+  }, [messageList]);
+
   return (
     <div className="messages">
       <Header />
       <div className="messages__list" id="message-list">
+        {generateMessageList}
+        {isTyping && <TypingMessage />}
       </div>
-      <Footer message={message} sendMessage={sendMessage} onChangeMessage={onChangeMessage} />
+      <Footer
+        message={message}
+        sendMessage={sendMessage}
+        onChangeMessage={onChangeMessage}
+      />
     </div>
   );
 }
